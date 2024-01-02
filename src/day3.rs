21f833e1e1740bf6f5dd244c48b0d101a232017a
pittengermdp <127773306+pittengermdp@users.noreply.github.com>
2024-01-01 18:56:45 -0600

    Starting day3.

---
+++ src/day3.rs
@@ -0,0 +1,18 @@
+use anyhow::Result;
+
+#[aoc(day3, part1)]
+#[must_use]
+pub fn part1(input: &str) -> u32 {
+    todo!()
+}
+
+#[aoc(day3, part2)]
+#[must_use]
+pub fn part2(input: &str) -> u64 {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
